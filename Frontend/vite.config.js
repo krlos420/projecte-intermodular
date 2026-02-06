832e33131f3b2bb074234bccbc2c8c73e39b1a07
Xx4rNaUxX <diablojuega@gmail.com>
2026-02-06 12:24:35 +0100

    arreglar errore menores en el acceso a puertos para hacer peticiones desde el front al back

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -15,4 +15,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    host: 'localhost',
+    port: 5173,
+    strictPort: true
+  }
 })
