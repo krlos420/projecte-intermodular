d3ec4a8b57929b1ff7d4ab0b8faeb916aaeb1541
Xx4rNaUxX <diablojuega@gmail.com>
2026-02-20 10:16:09 +0100

    arreglar error de clave foranea en migracion de lista de la compra y reset de bd

--- Backend/src/database/migrations/2026_02_17_115646_create_shopping_items_table.php
+++ Backend/src/database/migrations/2026_02_17_115646_create_shopping_items_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('shopping_items', function (Blueprint $table) {
             $table->id();
             $table->foreignId('house_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id_user')->on('users')->onDelete('cascade');
             $table->string('name');
             $table->string('quantity')->nullable(); // Ej: "1 Litro", "2 paquetes"
             $table->boolean('is_completed')->default(false);
